191a95962c799905c46b5cfae41ecef3982ebcff
praveenkumar725 <75404870+praveenkumar725@users.noreply.github.com>
2021-01-13 14:02:48 +0530

    Add files via upload

---
+++ skillrack.py
@@ -0,0 +1,40 @@
+# The program must accept two string values S1 and S2 as the input.
+# The program must print YES if the first three characters are equal in S1 and S2. Else the program must print NO as the output.
+
+
+S1=input("ENTER THE NAME :")
+S2=input("ENTER THE SECOND NAME :")
+if S1[0:3]==S2[0:3]:
+    print("YES")
+else:
+    print("NO")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
